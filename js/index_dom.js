ecb43d6782a40c9dbde9a6a318691c19339ee4a7
panzer34 <116589120+panzer34@users.noreply.github.com>
2023-05-24 10:26:30 -0400

    ejer

--- js/index_dom.js
+++ js/index_dom.js
@@ -1,6 +1,11 @@
+import scrollTopButton from "./dom/botton_scroll.js";
+import countdown from "./dom/cuenta_regresiva.js";
 import hamburguerMenu from "./dom/menu_hamburguesa.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
-import { shortcuts } from "./dom/teclado.js";
+import { moveBall, shortcuts } from "./dom/teclado.js";
+import darkTheme from "./dom/tema_oscuro.js";
+
+
 
 
 const d = document;
@@ -9,9 +14,13 @@ d.addEventListener("DOMContentLoaded", (e) => {
   hamburguerMenu(".panel-btn", ".panel", ".menu a");
   digitalClock("#reloj", "#activar-reloj", "#desactivar-reloj");
   alarm("assets/alarma.mp3", "#activar-alarma", "#desactivar-alarma");
+  countdown("countdown", "may 24, 2023 09:01:19", "Feliz Cumpleaños");
+  scrollTopButton(".scroll-top-btn");
+  darkTheme(".dark-theme-btn", "dark-mode");
 });
 
 d.addEventListener("keydown", (e) => {
   shortcuts(e);
+  moveBall(e, ".ball", ".stage");
 
 });
