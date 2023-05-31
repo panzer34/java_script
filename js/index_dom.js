2cd0acf954c5d70e3efad9693a72ab9dc327aa92
panzer34 <116589120+panzer34@users.noreply.github.com>
2023-05-31 10:56:50 -0400

    cuarto_commit

--- js/index_dom.js
+++ js/index_dom.js
@@ -10,9 +10,12 @@ import hamburguerMenu from "./dom/menu_hamburguesa.js";
 import responsiveMedia from "./dom/objeto_responsive.js";
 import responsiveTester from "./dom/prueba_responsive.js";
 import { digitalClock, alarm } from "./dom/reloj.js";
+import scrollSpy from "./dom/scroll_espia.js";
 import draw from "./dom/sorteo.js";
 import { moveBall, shortcuts } from "./dom/teclado.js";
 import darkTheme from "./dom/tema_oscuro.js";
+import contactFormValidations from "./dom/validaciones_formularios.js";
+import smartVideo from "./dom/video_inteligente.js";
 
 
 
@@ -62,7 +65,9 @@ d.addEventListener("DOMContentLoaded", (e) => {
   searchFilters(".card-filter", ".card");
   draw("#winner-btn", ".player");
   slider();
-
+  scrollSpy();
+  smartVideo();
+  contactFormValidations();
 });
 
 d.addEventListener("keydown", (e) => {
